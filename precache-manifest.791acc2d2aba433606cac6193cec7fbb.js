5dadba4f6cec507e225bc616bbc567842abb61dc
artemkopylov04 <artemkopylov04@gmail.com>
2019-04-25 16:33:35 +0300

    Updates

---
+++ precache-manifest.791acc2d2aba433606cac6193cec7fbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd64e34543e417c53ade63f31cf754b",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "b8411386d5e0a0d53d97",
+    "url": "/chat/static/css/2.f2beb6ae.chunk.css"
+  },
+  {
+    "revision": "6c3f75d103aca547b7e9",
+    "url": "/chat/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "b8411386d5e0a0d53d97",
+    "url": "/chat/static/js/2.4984bee4.chunk.js"
+  },
+  {
+    "revision": "6c3f75d103aca547b7e9",
+    "url": "/chat/static/js/main.3613e794.chunk.js"
+  },
+  {
+    "revision": "5bb637843dac5d879ad5",
+    "url": "/chat/static/js/runtime~main.54cd968c.js"
+  }
+]);
