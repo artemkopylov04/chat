088a49fbe30749c9ecd5c5a15a99508de03cc870
artemkopylov04 <artemkopylov04@gmail.com>
2019-04-25 16:32:47 +0300

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Widget, addResponseMessage } from 'react-chat-widget';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'react-chat-widget/lib/styles.css';
+
+class App extends Component {
+  componentDidMount() {
+    addResponseMessage("Welcome to this awesome chat!");
+  }
+
+  handleNewUserMessage = (newMessage) => {
+    console.log(`New message incoming! ${newMessage}`);
+    // Now send the message throught the backend API
+    addResponseMessage('some response');
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Widget
+          handleNewUserMessage={this.handleNewUserMessage}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
